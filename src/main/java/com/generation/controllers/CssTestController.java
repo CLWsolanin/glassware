fffb2dbb14e87624ad171663bedddd7450a16428
Federico Galuppi <locker9662@gmail.com>
2024-01-17 15:16:57 +0100

    v2

--- src/main/java/com/generation/controllers/CssTestController.java
+++ src/main/java/com/generation/controllers/CssTestController.java
@@ -9,11 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.generation.model.entities.Cup;
+import com.generation.model.entities.Glass;
+import com.generation.model.repositories.CupRepositoryMock;
+import com.generation.model.repositories.GlassRepositoryMock;
+import com.generation.model.repositories.GlasswareRepository;
+
 @WebServlet("/csstest")
 public class CssTestController extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/csstestw3.ftl").forward(req, resp);
+
+
+        GlasswareRepository<Cup> cupRepo = new CupRepositoryMock();
+        GlasswareRepository<Glass> glRepo = new GlassRepositoryMock();
+
+        Cup mostExpensiveCup = cupRepo.readAll().stream().sorted((c1,c2) -> (int)(c2.getPrice()-c1.getPrice())).findFirst().get();
+        Glass mostExpensiveGlass = glRepo.readAll().stream().sorted((c1,c2) -> (int)(c2.getPrice()-c1.getPrice())).findFirst().get();
+
+        //Ho i dati GREZZI, li impacchetto
+        req.setAttribute("cup", mostExpensiveCup);
+        req.setAttribute("glass", mostExpensiveGlass);
+        req.getRequestDispatcher("/homepageGlassware.ftl").forward(req, resp);
+
     }
 }
